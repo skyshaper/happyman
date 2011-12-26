9a7469508d0ada3745e0898bea0bb8f49409374e
Maximilian Gaß <mxey@mxey.net>
2011-12-26 23:40:32 +0100

    Implement beginning for HTTP SocketAnnouncer

--- lib/App/Happyman/Plugin/SocketAnnouncer.pm
+++ lib/App/Happyman/Plugin/SocketAnnouncer.pm
@@ -4,47 +4,29 @@ use Moose;
 
 with 'App::Happyman::Plugin';
 
-use AnyEvent::Handle;
-use AnyEvent::Socket;
-use Encode;
+use AnyEvent::HTTPD;
 
-has '_announcer_socket_guard' => (
-  is => 'ro',
-  builder => '_build_announcer_socket_guard',
+has '_httpd' => (
+    is => 'ro',
+    isa => 'AnyEvent::HTTPD',
+    builder => '_build_httpd',
 );
 
-sub _build_announcer_socket_guard {
-  my ($self) = @_;
-
-  return tcp_server(undef, 6666, sub {
-    my ($fh, $host, $port) = @_;
-    my $handle;
-    $handle = AnyEvent::Handle->new(
-      fh       => $fh,
-      on_eof   => sub { undef $handle },
-      on_error => sub { undef $handle },
+sub _build_httpd {
+    my ($self) = @_;
+    my $httpd = AnyEvent::HTTPD->new(port => 6666);
+    $httpd->reg_cb(
+        '/plain' => sub {
+            my ($httpd, $req) = @_;
+            $self->conn->send_notice( $req->parm('message') );
+            $req->respond([200, 'OK', {}, 'sent']);
+        },
+        '' => sub {
+            my ($httpd, $req) = @_;
+            $req->respond([404, 'Not Found', {}, 'Not Found']);
+        },
     );
-    $handle->push_read(line => sub {
-      my ($handle, $line, $eol) = @_;
-      $self->_announce($host, $handle, $line);
-    });
-  });
-}
-
-sub _announce {
-  my ($self, $host, $handle, $line) = @_;
-
-  $line = decode('UTF-8', $line);
-  $self->conn->send_notice("$line [$host]");
-
-  my $w;
-  $w = AE::timer(1, 0, sub {
-    $handle->push_read(line => sub {
-      my ($handle, $line, $eol) = @_;
-      $self->_announce($host, $handle, $line);
-      undef $w;
-    });
-  });
+    return $httpd;
 }
 
 __PACKAGE__->meta->make_immutable();
