d23de717aed165161a3015ed801e175ec075545d
Maximilian Gaß <mxey@mxey.net>
2012-12-21 19:38:09 +0100

    Strip some EOL whitespace

--- lib/App/Happyman/Plugin/SATQ.pm
+++ lib/App/Happyman/Plugin/SATQ.pm
@@ -30,7 +30,7 @@ has '_ua' => (
 
 sub _build_ua {
     my ($self) = @_;
-    
+
     my $ua = LWP::UserAgent->new();
 
     # LWP::UserAgent only sends an authenticated request after it sees a 401
@@ -39,7 +39,7 @@ sub _build_ua {
     my $authorization =  'Basic ' . encode_base64(
         $self->user . ':' . $self->password, '');
     $ua->default_header(Authorization => $authorization);
-    
+
     return $ua;
 }
 
@@ -47,18 +47,18 @@ sub _append_buffer {
     my ($self, $line) = @_;
 
     if (@{ $self->_buffer } >= 10) {
-        shift $self->_buffer; 
+        shift $self->_buffer;
     }
     push $self->_buffer, $line;
 }
 
 sub _post_quote {
     my ($self) = @_;
-    
+
     my $resp = $self->_ua->post($self->uri, {
         'quote[raw_quote]' => join("\n", @{ $self->_buffer }),
     });
-    
+
     if ($resp->code == 302) {
         return $resp->header('Location');
     }
@@ -69,12 +69,12 @@ sub _post_quote {
 
 sub on_message {
     my ($self, $msg) = @_;
-    
+
 
     if ($msg->full_text =~ /^\!quote\s*$/) {
         $msg->reply($self->_post_quote());
     }
-    
+
     my $line = sprintf('<%s> %s', $msg->sender_nick, $msg->full_text);
     $self->_append_buffer($line);
 
