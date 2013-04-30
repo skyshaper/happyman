a2825fc3175ea9bf23c4f7db8e64e8a7750a6994
Maximilian Gaß <mxey@mxey.net>
2013-05-01 01:02:08 +0200

    Use Mojolicious in SocketAnnouncer

--- lib/App/Happyman/Plugin/SocketAnnouncer.pm
+++ lib/App/Happyman/Plugin/SocketAnnouncer.pm
@@ -5,58 +5,53 @@ use Method::Signatures;
 
 with 'App::Happyman::Plugin';
 
-use AnyEvent::HTTPD;
 use JSON;
+use Mojolicious::Lite;
 use TryCatch;
 
-has '_httpd' => (
+has '_mojo' => (
     is      => 'ro',
-    isa     => 'AnyEvent::HTTPD',
-    builder => '_build_httpd',
+    isa     => 'Mojo::Server::Daemon',
+    builder => '_build_mojo',
 );
 
-method _build_httpd {
-    my $httpd = AnyEvent::HTTPD->new( host => '127.0.0.1', port => 6666 );
-    $httpd->reg_cb(
-        '/plain' => sub {
-            my ( $httpd, $req ) = @_;
-            $self->conn->send_notice( $req->parm('message') );
-            $req->respond( [ 200, 'OK', {}, 'sent' ] );
-        },
-        '/github' => sub {
-            my ( $httpd, $req ) = @_;
+method _build_mojo {
+    post '/plain' => func($app) {
+        say 'Receiving /plain';
+        $self->conn->send_notice( $app->param('message') );
+        $app->render(text => 'sent');
+    };
+    
+    post '/github' => func($app) {
+        my $data;
+        try {
+            $data = decode_json( $app->param('payload') );
+        }
+        catch {
+            $app->render(status => 400, text => 'JSON parsing failed');
+            return;
+        }
 
-            my $data;
-            try {
-                $data = decode_json( $req->parm('payload') );
-            }
-            catch {
-                $req->respond(
-                    [ 400, 'Bad Request', {}, 'JSON decode failed' ] );
-                return;
-            }
+        foreach my $commit ( @{ $data->{commits} } ) {
+            my $message = sprintf(
+                "%s/%s (%s): %s - %s: %s\n",
+                $data->{repository}->{owner}->{name},
+                $data->{repository}->{name},
+                ( split( qr{/}, $data->{ref} ) )[-1],
+                $commit->{author}->{name},
+                substr( $commit->{id}, 0, 8 ),
+                ( split( qr{\n}, $commit->{message} ) )[0],
+            );
+            $self->conn->send_notice($message);
+        }
 
-            foreach my $commit ( @{ $data->{commits} } ) {
-                my $message = sprintf(
-                    "%s/%s (%s): %s - %s: %s\n",
-                    $data->{repository}->{owner}->{name},
-                    $data->{repository}->{name},
-                    ( split( qr{/}, $data->{ref} ) )[-1],
-                    $commit->{author}->{name},
-                    substr( $commit->{id}, 0, 8 ),
-                    ( split( qr{\n}, $commit->{message} ) )[0],
-                );
-                $self->conn->send_notice($message);
-            }
-
-            $req->respond( [ 200, 'OK', {}, 'sent' ] );
-        },
-        '' => sub {
-            my ( $httpd, $req ) = @_;
-            $req->respond( [ 404, 'Not Found', {}, 'Not Found' ] );
-        },
-    );
-    return $httpd;
+        $app->render(status => 200, text => 'sent');
+        return;
+    },
+    
+    my $daemon = Mojo::Server::Daemon->new(app => app, listen => ['http://*:6666']);
+    $daemon->start();
+    return $daemon;
 }
 
 __PACKAGE__->meta->make_immutable();
