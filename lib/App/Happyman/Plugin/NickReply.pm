b35099d6b64497f76040c7f6c35671670e772a62
Maximilian Gaß <mxey@mxey.net>
2013-04-30 18:10:39 +0200

    Remove Coro from NickReply

--- lib/App/Happyman/Plugin/NickReply.pm
+++ lib/App/Happyman/Plugin/NickReply.pm
@@ -4,14 +4,17 @@ use Moose;
 
 with 'App::Happyman::Plugin';
 
-use Coro::AnyEvent;
+use AnyEvent;
 
 sub on_message {
     my ($self, $msg) = @_;
 
     if ($msg->full_text eq $self->conn->nick) {
-        Coro::AnyEvent::sleep rand(2);
-        $self->conn->send_message($msg->sender_nick);
+        my $timer;
+        $timer = AE::timer rand(2), 0, sub {
+            undef $timer;
+            $self->conn->send_message($msg->sender_nick);
+        };
     }
 }
 
