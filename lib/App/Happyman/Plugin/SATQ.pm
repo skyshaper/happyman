4218b0bda60a1212c81301eee0d9376a30061057
Maximilian Gaß <mxey@mxey.net>
2013-07-31 22:22:28 +0200

    SATQ: Extract method _upload_quote_to_satq

--- lib/App/Happyman/Plugin/SATQ.pm
+++ lib/App/Happyman/Plugin/SATQ.pm
@@ -30,31 +30,36 @@ sub _push_line_to_buffer {
     push $self->_buffer, $line;
 }
 
+sub _upload_quote_to_satq {
+    my ($self, $quote_command_message) = @_;
+    my $authorization = 'Basic '
+        . encode_base64( $self->user . ':' . $self->password, '' );
+    my $headers = { Authorization => $authorization };
+    my $form
+        = { 'quote[raw_quote]' => join( "\n", @{ $self->_buffer } ) };
+    $self->_log_debug( 'Posting quote: ' . Dumper($form) );
+
+    $self->_ua->post(
+        $self->uri,
+        $headers,
+        form => $form,
+        sub {
+            my ( undef, $tx ) = @_;
+            if ( $tx->error ) {
+                $self->_log( $tx->error );
+                $quote_command_message->reply_on_channel( $tx->error );
+            }
+            $self->_log( $tx->res->headers->location || $tx->res->code );
+            $quote_command_message->reply_on_channel( $tx->res->headers->location
+                    || $tx->res->code );
+        }
+    );
+}
+
 sub on_message {
     my ( $self, $msg ) = @_;
     if ( $msg->full_text =~ /^\!quote\s*$/ ) {
-        my $authorization = 'Basic '
-            . encode_base64( $self->user . ':' . $self->password, '' );
-        my $headers = { Authorization => $authorization };
-        my $form
-            = { 'quote[raw_quote]' => join( "\n", @{ $self->_buffer } ) };
-        $self->_log_debug( 'Posting quote: ' . Dumper($form) );
-
-        $self->_ua->post(
-            $self->uri,
-            $headers,
-            form => $form,
-            sub {
-                my ( undef, $tx ) = @_;
-                if ( $tx->error ) {
-                    $self->_log( $tx->error );
-                    $msg->reply_on_channel( $tx->error );
-                }
-                $self->_log( $tx->res->headers->location || $tx->res->code );
-                $msg->reply_on_channel( $tx->res->headers->location
-                        || $tx->res->code );
-            }
-        );
+        $self->_upload_quote_to_satq($msg);
     }
 
     $self->_push_line_to_buffer(sprintf( '<%s> %s', $msg->sender_nick, $msg->full_text ));
