67e4f9b7ebee573efc199ccf74ab1a99ee3ae81e
Maximilian Gaß <mxey@mxey.net>
2013-08-10 13:58:25 +0200

    Make deploy resistant to non-continous Git history

--- fabfile.py
+++ fabfile.py
@@ -7,7 +7,7 @@ env.hosts = [
 def deploy():
     local('git push')
     with cd('/srv/happyman/happyman'):
-        run('su happyman -c "git pull"')
+        run('su happyman -c "git remote update && git reset --hard origin/master"')
         run('su happyman -c "carton install --deployment"')
     execute(restart)
 
