7707a81c614f259429f72bbc93940c3de241be36
Maximilian Gaß <mxey@mxey.net>
2013-08-16 19:50:37 +0200

    Change hosts again

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,24 @@
 from fabric.api import *
 
 env.hosts = [
-    'root@mxeyweb.mxey.net'
+    'shaperia@lynx.uberspace.de'
 ]
 
+env.target_directory = './happyman'
+
 def init():
-    with cd('/srv/happyman/happyman'):
+    run('git clone -q https://github.com/skyshaper/happyman.git ' + env.target_directory)
+    with cd(env.target_directory):
         run('virtualenv python_virtualenv')
 
 def deploy():
     local('git push')
-    with cd('/srv/happyman/happyman'):
-        run('su happyman -c "git remote update && git reset --hard origin/master"')
-        run('su happyman -c "carton install --deployment"')
+    with cd(env.target_directory):
+        run('git remote update && git reset --hard origin/master')
+        run('carton install --deployment')
         with cd('python_virtualenv'):
             run('./bin/pip install -r ../cobe_python_requirements.txt')
     execute(restart)
 
 def restart():
-    run('supervisorctl restart happyman')
+    run('svc -t ~/service/happyman')
