258c31c3b3f9cbeca6904c7785785d767821886e
Maximilian Gaß <mxey@mxey.net>
2013-12-22 13:12:59 +0100

    Hug: Use subtype for trimming text

--- lib/App/Happyman/Action.pm
+++ lib/App/Happyman/Action.pm
@@ -3,17 +3,27 @@ use v5.18;
 use Moose;
 use namespace::autoclean;
 
-has [qw(text sender_nick)] => (
+use Moose::Util::TypeConstraints;
+
+subtype 'TrimmedStr'
+    => as 'Str'
+    => where { /^\S .* \S$/x };
+
+coerce 'TrimmedStr'
+    => from 'Str'
+    => via { s/^\s+ | \s+$//gxr };
+
+
+has 'sender_nick' => (
     is       => 'ro',
     isa      => 'Str',
     required => 1,
 );
-
-sub BUILDARGS {
-    my ( $self, %args ) = @_;
-    
-    $args{text} =~ s/^\s+|\s+$//g;
-    return \%args;
-}
+has 'text' => (
+    is       => 'ro',
+    isa      => 'TrimmedStr',
+    required => 1,
+    coerce   => 1,
+);
 
 1;
