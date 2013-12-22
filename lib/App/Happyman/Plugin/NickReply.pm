d689ce2e2519a3520ed5ee713efceccb2a80b057
Maximilian Gaß <mxey@mxey.net>
2013-12-22 13:17:44 +0100

    NickReply: inline _do_nick_reply

--- lib/App/Happyman/Plugin/NickReply.pm
+++ lib/App/Happyman/Plugin/NickReply.pm
@@ -7,32 +7,19 @@ with 'App::Happyman::Plugin';
 
 use AnyEvent;
 
-sub _do_nick_reply {
-    my ( $self, $text, $needed_text, $sender_nick, $action ) = @_;
-
-    if ( $text eq $needed_text ) {
-        $self->_log( "Triggered by " . $sender_nick );
-
+sub on_message {
+    my ( $self, $msg ) = @_;
+    
+    if ( $msg->full_text eq $self->conn->nick ) {
+        $self->_log( "Triggered by " . $msg->sender_nick );
+    
         my $timer;
         $timer = AE::timer rand(2), 0, sub {
             undef $timer;
-            $self->_log( "Replying to " . $sender_nick );
-            $action->();
-            }
-    }
-}
-
-sub on_message {
-    my ( $self, $msg ) = @_;
-
-    $self->_do_nick_reply(
-        $msg->full_text,
-        $self->conn->nick,
-        $msg->sender_nick,
-        sub {
+            $self->_log( "Replying to " . $msg->sender_nick );
             $self->conn->send_message_to_channel( $msg->sender_nick );
-        }
-    );
+        };
+    }
 }
 
 __PACKAGE__->meta->make_immutable();
