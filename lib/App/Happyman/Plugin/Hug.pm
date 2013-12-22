b32e8c04e68964e51973992a53a097d9d36bba8f
Maximilian Gaß <mxey@mxey.net>
2013-12-22 13:20:49 +0100

    Hug: inline _do_nick_reply

--- lib/App/Happyman/Plugin/Hug.pm
+++ lib/App/Happyman/Plugin/Hug.pm
@@ -7,33 +7,20 @@ with 'App::Happyman::Plugin';
 
 use AnyEvent;
 
-sub _do_nick_reply {
-    my ( $self, $text, $needed_text, $sender_nick, $action ) = @_;
-
-    if ( $text eq $needed_text ) {
-        $self->_log( "Triggered by " . $sender_nick );
-
+sub on_action {
+    my ( $self, $action ) = @_;
+    
+    if ( $action->text eq 'hugs ' . $self->conn->nick ) {
+        $self->_log( 'Hugged by ' . $action->sender_nick );
+    
         my $timer;
         $timer = AE::timer rand(2), 0, sub {
             undef $timer;
-            $self->_log( "Replying to " . $sender_nick );
-            $action->();
-            }
-    }
-}
-
-sub on_action {
-    my ( $self, $action ) = @_;
-
-    $self->_do_nick_reply(
-        $action->text,
-        "hugs " . $self->conn->nick,
-        $action->sender_nick,
-        sub {
+            $self->_log( 'Hugging ' . $action->sender_nick );
             $self->conn->send_action_to_channel(
-                "hugs " . $action->sender_nick );
-        }
-    );
+                'hugs ' . $action->sender_nick );
+        };
+    }
 }
 
 __PACKAGE__->meta->make_immutable();
